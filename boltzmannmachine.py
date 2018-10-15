5fdaf8238f527b12bde0ff00b76bb42688ff9983
Natália Raythz <nraythz@gmail.com>
2018-10-15 14:22:47 -0300

    Update boltzmannmachine.py

--- boltzmannmachine.py
+++ boltzmannmachine.py
@@ -1,13 +1,13 @@
-import torch # para Deep Learning
-import torch.autograd as autograd # para autodiferenciação
-import torch.nn as nn # para montar redes neurais
-import torch.nn.functional as F # funções do Torch
-import torch.optim as optim # para otimização com GDE
-import numpy as np # para
-# carrega os dados MNIST
+import torch 
+import torch.autograd as autograd
+import torch.nn as nn 
+import torch.nn.functional as F 
+import torch.optim as optim 
+import numpy as np 
+
 from tensorflow.examples.tutorials.mnist import input_data
 data = input_data.read_data_sets("tmp/", one_hot=False)
-data = np.random.permutation(data.train.images) # só precisamos das imagens aqui
+data = np.random.permutation(data.train.images) 
 
 class RBM(nn.Module):
     def __init__(self, nv=28*28, nh=512, cd_steps=1):
@@ -16,7 +16,7 @@ class RBM(nn.Module):
         self.W = nn.Parameter(torch.randn(nv, nh) * 0.01)
         self.bv = nn.Parameter(torch.zeros(nv))
         self.bh = nn.Parameter(torch.zeros(nh))
-        self.cd_steps = cd_steps # define a forma de Contrastive Divergence
+        self.cd_steps = cd_steps 
 
     def bernoulli(self, p):
         # return F.relu(torch.sign(p - autograd.Variable(torch.rand(p.size()).cuda())))  
@@ -38,7 +38,7 @@ class RBM(nn.Module):
 
     def forward(self, v):
         vk = v.clone() # inicializa vk
-        # realiza k passos de amostragem de Gibbs
+      
         for step in range(self.cd_steps): 
             hk = self.sample_h(vk)
             vk = self.sample_v(hk)
@@ -46,30 +46,30 @@ class RBM(nn.Module):
         return v, vk.detach()
     
 rbm = RBM()
-# rbm.cuda() # move os parâmetros da rede para a GPU
+
 optimizer = optim.Adam(rbm.parameters(), 0.001)
 
-batch_size = 64 # tamanho do mini-lote
-epochs = 25 # qtd de épocas de treinamento
+batch_size = 64 
+epochs = 25 
 for epoch in range(epochs):
     losses = []
-    # loop de treinamento
+    
     for i in range(0, len(data)-batch_size, batch_size):
-        # cria os mini-lotes
+     
         x_batch = data[i:i+batch_size]
         x_batch = torch.from_numpy(x_batch).float()
-        # x_batch = x_batch.cuda()
+        
         x_batch = autograd.Variable(x_batch).bernoulli()
 
-        optimizer.zero_grad() # zera os gradientes computados anteriormente
-        v, vk = rbm(x_batch) # realiza o forward-pass (CD com amostragens de Gibbs)
-        loss = rbm.energy(v) - rbm.energy(vk) # computa o custo
+        optimizer.zero_grad() 
+        v, vk = rbm(x_batch)
+        loss = rbm.energy(v) - rbm.energy(vk) 
         losses.append(loss.data[0])
 
-        loss.backward() # realiza o backward-pass
-        optimizer.step() # atualiza os parâmetros
+        loss.backward() 
+        optimizer.step() 
     
     print('Custo na época %d: ' % epoch, np.mean(losses))
     if epoch % 5 == 0 and epoch > 0: # a cada 5 épocas
-        rbm.cd_steps += 2 # aumenta os as iterações em CD
+        rbm.cd_steps += 2
         print('Alterando para CD%d...' % rbm.cd_steps)
